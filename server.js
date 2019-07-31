5485cf6c15c6dc5209a096ecda73e02b7f2d75da
itstheonlychris <chris@pivvot.co>
2019-07-31 16:47:46 -0500

    Set up for production test - added postbuild script and changed server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const slackbot = require('./routes/api/slackbot');
 const tasks = require('./routes/api/tasks');
+const path = require('path');
 
 const app = express();
 
@@ -18,6 +19,16 @@ app.get('/', (req, res) => {
 app.use('/api/slackbot', slackbot);
 
 app.use('/api/tasks', tasks);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const server = app.listen(process.env.PORT || PORT, () => {
 	console.log(`Bot is listening on port ${PORT}`);
